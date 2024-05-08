7d455061112f1a7a4aeec7dedbc8c5590668e766
hetdagli234 <daglihet@gmail.com>
2024-05-08 19:29:12 +0530

    Adding basic home page

---
+++ app/solflix-react-app/src/components/Home.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+
+const Home: FC = () => {
+  return (
+    <div className="home">
+      <h1>Welcome to SolFlix</h1>
+      {/* Add your movie list, details, etc. here */}
+    </div>
+  );
+};
+
+export default Home;
