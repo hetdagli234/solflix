2a26d7c50ed1308f769db13409f089752d40658f
hetdagli234 <daglihet@gmail.com>
2024-05-08 19:25:35 +0530

    Added App.tsx

---
+++ app/solflix-react-app/src/App.tsx
@@ -0,0 +1,49 @@
+import React, { FC, useMemo } from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { clusterApiUrl } from '@solana/web3.js';
+import {
+  PhantomWalletAdapter,
+  SlopeWalletAdapter,
+  SolflareWalletAdapter,
+} from '@solana/wallet-adapter-wallets';
+import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+
+import './App.css';
+import Home from './components/Home';
+
+const App: FC = () => {
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new SlopeWalletAdapter(),
+      new SolflareWalletAdapter(),
+    ],
+    []
+  );
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <div className="app">
+            <nav>
+              <div className="nav-left">
+                <img src="solflix_logo.png" alt="SolFlix Logo" />
+              </div>
+              <div className="nav-right">
+                <WalletMultiButton />
+              </div>
+            </nav>
+            <Home />
+          </div>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
+export default App;
