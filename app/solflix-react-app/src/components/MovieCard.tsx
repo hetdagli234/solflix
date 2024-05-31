23711e25580a9632fa650d58af52daf9740af839
hetdagli234 <daglihet@gmail.com>
2024-05-31 20:09:28 +0530

    Movie Card examples

--- app/solflix-react-app/src/components/MovieCard.tsx
+++ app/solflix-react-app/src/components/MovieCard.tsx
@@ -19,11 +19,20 @@ const MovieCard: FC<MovieCardProps> = ({ movie }) => {
       <h3>{movie.title}</h3>
       {expanded && (
         <div className="movie-details">
-          {/* Add movie details, trailer, etc. */}
+          <h2>{movie.title}</h2>
+          <p>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+            title="Dummy Trailer"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          ></iframe>
         </div>
       )}
     </div>
   );
 };
 
+export default MovieCard;
 export default MovieCard;
